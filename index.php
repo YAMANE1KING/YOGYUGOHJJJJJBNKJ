e9c94b42a48ad64e51965754852df2aa7172ed49
YAMANE1KING <legendtradingai@gmail.com>
2026-02-02 20:25:03 +0100

    Enhance error reporting in index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-echo "DEBUG: index.php reached successfully.<br>";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+if (!file_exists(__DIR__.'/vendor/autoload.php')) {
+    die('Composer autoload.php not found. Please run composer install.');
+}
+
 require __DIR__.'/vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 $mail = new PHPMailer;
