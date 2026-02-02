d293fe44b3f88b0e1fa91b68a48a66ad02e2507f
YAMANE1KING <legendtradingai@gmail.com>
2026-02-02 22:26:24 +0100

    Add debug info to test_php.php

--- test_php.php
+++ test_php.php
@@ -1,3 +1,11 @@
 <?php
-echo "PHP is working. Server time: " . date('Y-m-d H:i:s');
+require __DIR__.'/vendor/autoload.php';
+$config = require __DIR__.'/app/config.php';
+
+echo "PHP is working. Server time: " . date('Y-m-d H:i:s') . "<br>";
+echo "CWD: " . getcwd() . "<br>";
+echo "PATH constant (from config): " . (defined('PATH') ? PATH : 'NOT DEFINED') . "<br>";
+echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "<br>";
+echo "QUERY_STRING: " . $_SERVER['QUERY_STRING'] . "<br>";
+
 ?>
