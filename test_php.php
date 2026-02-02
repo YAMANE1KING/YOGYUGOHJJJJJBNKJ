0220b7a38e51f9f1c0f63220b492a8e513cd9d82
YAMANE1KING <legendtradingai@gmail.com>
2026-02-02 23:33:25 +0100

    Update debug script to inspect init.php

--- test_php.php
+++ test_php.php
@@ -6,6 +6,15 @@ echo "PHP is working. Server time: " . date('Y-m-d H:i:s') . "<br>";
 echo "CWD: " . getcwd() . "<br>";
 echo "PATH constant (from config): " . (defined('PATH') ? PATH : 'NOT DEFINED') . "<br>";
 echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "<br>";
-echo "QUERY_STRING: " . $_SERVER['QUERY_STRING'] . "<br>";
+
+echo "<h4>Inspecting app/init.php:</h4>";
+$init_file = __DIR__.'/app/init.php';
+if (file_exists($init_file)) {
+    $content = file_get_contents($init_file);
+    echo "<pre>" . htmlspecialchars(substr($content, 0, 2000)) . "</pre>";
+} else {
+    echo "app/init.php NOT FOUND at $init_file";
+}
+
 
 ?>
