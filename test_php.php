4f1c43fca6588c4bb70f84b2a5d27c61f2aeec23
YAMANE1KING <legendtradingai@gmail.com>
2026-02-03 00:51:49 +0100

    List files in test_php.php

--- test_php.php
+++ test_php.php
@@ -12,14 +12,9 @@ echo "CWD: " . getcwd() . "<br>";
 echo "PATH constant (from config): " . (defined('PATH') ? PATH : 'NOT DEFINED') . "<br>";
 echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "<br>";
 
-echo "<h4>Inspecting pro.php:</h4>";
-$pro_file = __DIR__.'/pro.php';
-if (file_exists($pro_file)) {
-    $content = file_get_contents($pro_file);
-    echo "<pre>" . htmlspecialchars(substr($content, 0, 10000)) . "</pre>";
-} else {
-    echo "pro.php NOT FOUND at $pro_file";
-}
+echo "<h4>Directory Listing:</h4>";
+$files = scandir(__DIR__);
+echo "<pre>" . print_r($files, true) . "</pre>";
 
 
 ?>
