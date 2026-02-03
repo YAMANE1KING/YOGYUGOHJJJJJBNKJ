68dd975be4f0eda6fa0c1ac05663a96007977ff1
YAMANE1KING <legendtradingai@gmail.com>
2026-02-03 01:21:38 +0100

    Update server_debug.php with explicit SQL file checks

--- server_debug.php
+++ server_debug.php
@@ -12,6 +12,12 @@ echo "CWD: " . getcwd() . "<br>";
 echo "PATH constant (from config): " . (defined('PATH') ? PATH : 'NOT DEFINED') . "<br>";
 echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "<br>";
 
+echo "<h3>Specific File Checks:</h3>";
+$sql_files = ['schema.pg.sql', 'seed.pg.sql', 'closestar.sql'];
+foreach ($sql_files as $sf) {
+    echo "$sf: " . (file_exists(__DIR__.'/'.$sf) ? 'FOUND' : 'NOT FOUND') . "<br>";
+}
+
 echo "<h4>Directory Listing:</h4>";
 $files = scandir(__DIR__);
 echo "<pre>" . print_r($files, true) . "</pre>";
